f96c4af529b2d180013e83a6e567d7ebca481cb3
Migsar Navarro <dev@migsar.com>
2025-12-02 22:15:47 +0000

    test fastmcp.cloud

--- mcp_quiz/server.py
+++ mcp_quiz/server.py
@@ -1,7 +1,7 @@
 from fastmcp import FastMCP
+from .supabase import client as sb
 
 from .handlers import (
-    list_quizzes,
     create_quiz,
     get_quiz,
     delete_quiz
@@ -14,7 +14,12 @@ def healthz() -> str:
     return f"Quiz MCP!"
 
 #dev only, at least we need to restrict the listed quizzes with auth but it is not implemented
-server.tool(list_quizzes)
+@server.tool
+def list_quizzes():
+    """
+    Get all quizzes.
+    """
+    return sb.table("quizzes").select("*").execute()
 
 server.tool(create_quiz)
 server.tool(get_quiz)
